32da7c48109d62e9f81e6333d6368891ff206b7b
Vy Khong <Khongtuongvy09@gmail.com>
2026-01-24 16:09:56 -0500

    Add ALU class header with ADD and MUL methods

---
+++ headers/ALU.h
@@ -0,0 +1,12 @@
+#ifndef ALU_h
+#define ALU_h
+#include <cstdint>
+
+class ALU {
+public:
+    static uint16_t ADD(uint16_t a, uint16_t b);
+    static uint16_t MUL(uint16_t a, uint16_t b);
+
+};
+
+#endif
